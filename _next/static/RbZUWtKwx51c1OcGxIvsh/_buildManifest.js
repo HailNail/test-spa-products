860983cb622f7de53147d9e54efad31e92c6f616
Nail Gati <theproducer228@gmail.com>
2025-11-06 15:48:31 +0500

    Updates

--- _next/static/RbZUWtKwx51c1OcGxIvsh/_buildManifest.js
+++ _next/static/RbZUWtKwx51c1OcGxIvsh/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/test-spa-products//_next/:path+",
+        "destination": "/test-spa-products/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
